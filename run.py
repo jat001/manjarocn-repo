42e4b2647512876b276b9f1909cce961528b5b5f
Jat <jat@sinosky.org>
2021-04-11 03:26:47 +0800

    add blacklist

--- run.py
+++ run.py
@@ -5,10 +5,11 @@ import sys
 import re
 import subprocess
 from pathlib import Path
+from datetime import datetime
 
 
 def prin(*args, pkg=None, error=False, **kwargs):
-    print(':point_right: ManjaroCN --->', *args,
+    print(datetime.now(), ':point_right: ManjaroCN --->', *args,
           '%s --->' % pkg if pkg is not None else '',
           file=sys.stderr if error else sys.stdout, **kwargs)
 
@@ -44,10 +45,16 @@ def parse_env():
     }
 
 
-def build(pkg, env, errors, depends_tree=None):
+def build(pkg, env, blacklist, errors, depends_tree=None):
     pkgbuild = env['paths']['ARCHCN'][0] / pkg / 'PKGBUILD'
     if not pkgbuild.is_file():
         errors.append([pkg, 'PKGBUILD not found'])
+        prin('build failed: PKGBUILD not found', pkg=pkg, error=True)
+        return False
+
+    if pkg in blacklist:
+        errors.append([pkg, 'package in blacklist'])
+        prin('build failed: package in blacklist', pkg=pkg, error=True)
         return False
 
     if depends_tree is None:
@@ -68,7 +75,7 @@ def build(pkg, env, errors, depends_tree=None):
     if depends:
         prin('found depends:', depends, pkg=pkg)
         for i in depends:
-            if not build(i, env, errors, depends_tree):
+            if not build(i, env, blacklist, errors, depends_tree):
                 prin('build depend %s failed:' % i, pkg=pkg, error=True)
                 return False
 
@@ -96,12 +103,15 @@ def main():
     env = parse_env()
     prin('Build env vars:', env)
 
+    with (Path(__file__).parent / 'blacklist.txt').open(encoding='utf8') as f:
+        blacklist = {i.strip() for i in f}
+
     errors = []
     with (Path(__file__).parent / 'list.txt').open(encoding='utf8') as f:
         for i in f:
             i = env['paths']['ARCHCN'][0].glob(i.strip())
             for j in i:
-                build(j.name, env, errors)
+                build(j.name, env, blacklist, errors)
 
     if errors:
         prin('Build errors:', error=True)
